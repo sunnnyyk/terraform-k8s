65a23402bfa4e02e4c361b2aa067e3161c1f4e03
sunnnyyk <153540154+sunnnyyk@users.noreply.github.com>
2025-06-28 12:33:34 +0530

    Update variable.tf

--- php/privateroutetable/variable.tf
+++ php/privateroutetable/variable.tf
@@ -4,21 +4,11 @@ variable "vpc_id" {
   default = ""
 }
 
-# variable "privatesub_id" {
-#     type = string
-
-#   default = ""
-# }
 variable "natgate_id" {
     type = string
 
   default = ""
 }
-
-# variable "rt_name1" {
-#   type    = string
-#   default = "ninja-rt-pvt"
-# }
 
 variable "rt_cidr" {
   type    = string
