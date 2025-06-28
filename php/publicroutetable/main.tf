fe82f89261efc85c0799e888e212fd5a3ae4721d
sunnnyyk <153540154+sunnnyyk@users.noreply.github.com>
2025-06-28 12:30:57 +0530

    Update main.tf

--- php/publicroutetable/main.tf
+++ php/publicroutetable/main.tf
@@ -10,8 +10,3 @@ resource "aws_route" "rt" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = var.intnet_gateway_id
 }
-
-# resource "aws_route_table_association" "rta" {
-#   subnet_id      = var.sub_id
-#   route_table_id = aws_route_table.route-table.id
-# }
