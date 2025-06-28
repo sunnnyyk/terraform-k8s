b783c913409ef48c54d377b5725a1dbdb55e5495
sunnnyyk <153540154+sunnnyyk@users.noreply.github.com>
2025-06-28 12:31:26 +0530

    Update variable.tf

--- php/publicroutetable/variable.tf
+++ php/publicroutetable/variable.tf
@@ -3,20 +3,9 @@ variable "intnet_gateway_id" {
 
   default = ""
 }
+
 variable "vpc_id" {
     type = string
 
   default = ""
 }
-# variable "sub_id" {
-#     type = string
-
-#   default = ""
-# }
-
-
-# variable "publicroutetable/main.tf1" {
-#     type = string
-
-#   default = "0.0.0.0/0"
-# }
